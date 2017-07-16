15b9a5b550c1851b15d2b8efb01c5698b068227d
Tadesse Tegena <tadessetegena@Tadesses-MacBook-Pro.local>
2017-07-16 18:00:52 -0400

    adding files

---
+++ assets/javascript/game.js
@@ -0,0 +1,98 @@
+$( document ).ready(function(){
+  var Random=Math.floor(Math.random()*100+1)
+  
+  // Selects a random number to be shown at the start of the game
+  // Numbers are between 1 - 50
+  
+  $('#randomNumber').text(Random);
+
+  // Appending random number to the randomNumber id in the html doc
+  
+  var num1= Math.floor(Math.random()*9+1)
+  var num2= Math.floor(Math.random()*9+1)
+  var num3= Math.floor(Math.random()*9+1)
+  var num4= Math.floor(Math.random()*9+1)
+  // Setting up random numbers for each crystals
+  // Random number has to be between 1 - 10.
+  // 
+  var userTotal= 0; 
+  var wins= 0;
+  var losses = 0;
+  
+$('#numberWins').text(wins);
+$('#numberLosses').text(losses);
+//resets the game
+function reset(){
+      Random=Math.floor(Math.random()*100+1);
+      console.log(Random)
+      $('#randomNumber').text(Random);
+      num1= Math.floor(Math.random()*11+1);
+      num2= Math.floor(Math.random()*11+1);
+      num3= Math.floor(Math.random()*11+1);
+      num4= Math.floor(Math.random()*11+1);
+      userTotal= 0;
+      $('#finalTotal').text(userTotal);
+      } 
+//adds the wins to the userTotal
+function bravo(){
+alert("You won!");
+  wins++; 
+  $('#numberWins').text(wins);
+  reset();
+}
+//addes the losses to the userTotal
+function loser(){
+alert ("You lose!");
+  losses++;
+  $('#numberLosses').text(losses);
+  reset()
+}
+//sets up click for crystals
+  $('#one').on ('click', function(){
+    userTotal = userTotal + num1;
+    console.log("New userTotal " + userTotal);
+    $('#finalTotal').text(userTotal); 
+          //sets win/lose conditions
+        if (userTotal == Random){
+          bravo();
+        }
+        else if ( userTotal > Random){
+          loser();
+        }   
+  })  
+  $('#two').on ('click', function(){
+    userTotal = userTotal + num2;
+    console.log("New userTotal " + userTotal);
+    $('#finalTotal').text(userTotal); 
+        if (userTotal == Random){
+          bravo();
+        }
+        else if ( userTotal > Random){
+          loser();
+        } 
+  })  
+  $('#three').on ('click', function(){
+    userTotal = userTotal + num3;
+    console.log("New userTotal " + userTotal);
+    $('#finalTotal').text(userTotal);
+//sets win/lose conditions
+          if (userTotal == Random){
+          bravo();
+        }
+        else if ( userTotal > Random){
+          loser();
+        } 
+  })  
+  $('#four').on ('click', function(){
+    userTotal = userTotal + num4;
+    console.log("New userTotal " + userTotal);
+    $('#finalTotal').text(userTotal); 
+      
+          if (userTotal == Random){
+          bravo();
+        }
+        else if ( userTotal > Random){
+          loser();
+        }
+  });   
+}); 
