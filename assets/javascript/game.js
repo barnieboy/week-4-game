8e0f431e1042736ec86de70b67c4338f94cbafdc
Tadesse Tegena <tadessetegena@Tadesses-MacBook-Pro.local>
2017-07-17 17:47:11 -0400

    add files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 $( document ).ready(function(){
-  var Random=Math.floor(Math.random()*100+1)
+  var Random = Math.floor(Math.random()*100+1)
   
   // Selects a random number to be shown at the start of the game
   // Numbers are between 1 - 50
@@ -8,28 +8,28 @@ $( document ).ready(function(){
 
   // Appending random number to the randomNumber id in the html doc
   
-  var num1= Math.floor(Math.random()*9+1)
-  var num2= Math.floor(Math.random()*9+1)
-  var num3= Math.floor(Math.random()*9+1)
-  var num4= Math.floor(Math.random()*9+1)
+  var num1 = Math.floor(Math.random()*11+1)
+  var num2 = Math.floor(Math.random()*11+1)
+  var num3 = Math.floor(Math.random()*11+1)
+  var num4 = Math.floor(Math.random()*11+1)
   // Setting up random numbers for each crystals
   // Random number has to be between 1 - 10.
   // 
-  var userTotal= 0; 
-  var wins= 0;
+  var userTotal = 0; 
+  var wins = 0;
   var losses = 0;
   
 $('#numberWins').text(wins);
 $('#numberLosses').text(losses);
 //resets the game
 function reset(){
-      Random=Math.floor(Math.random()*100+1);
+      Random = Math.floor(Math.random()*100+1);
       console.log(Random)
       $('#randomNumber').text(Random);
-      num1= Math.floor(Math.random()*11+1);
-      num2= Math.floor(Math.random()*11+1);
-      num3= Math.floor(Math.random()*11+1);
-      num4= Math.floor(Math.random()*11+1);
+      num1 = Math.floor(Math.random()*11+1);
+      num2 = Math.floor(Math.random()*11+1);
+      num3 = Math.floor(Math.random()*11+1);
+      num4 = Math.floor(Math.random()*11+1);
       userTotal= 0;
       $('#finalTotal').text(userTotal);
       } 
